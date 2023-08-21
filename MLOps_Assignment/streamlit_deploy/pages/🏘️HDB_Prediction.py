27cce5a0b9db1963a6cbc9313757cc97632ea28f
ShermainePeh08 <pehshermaine@gmail.com>
2023-08-21 19:35:51 +0800

    deployment updatee

--- MLOps_Assignment/streamlit_deploy/pages/🏘️HDB_Prediction.py
+++ MLOps_Assignment/streamlit_deploy/pages/🏘️HDB_Prediction.py
@@ -86,4 +86,4 @@ if __name__ == "__main__":
         prediction = predict_hdb(hdb_model, df)
         st.subheader('Predicted Output')
         st.markdown("<h1 style='color: green;'>🤑💸</h1>", unsafe_allow_html=True)
-        st.write('The predicted price of your HDB rounded to nearest thousands is: $', str(round(prediction, -3)))
+        st.write('The predicted price of your HDB rounded to nearest thousands is: $'+ str(round(prediction, -3)))
